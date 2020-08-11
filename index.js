e692c1d9ce1355cf14571c619574d33a35d82c09
jaicedev <jaicedev@gmail.com>
2020-08-11 12:32:07 -0400

    Some Functionality Complete

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
-let api_key = 'CoPdyLHNoI77xjEjFBWBjw0Qhy6PJjOvERyZMMo1'
-let url = 'https://developer.nps.gov/api/v1/parks/?'
+const api_key = 'CoPdyLHNoI77xjEjFBWBjw0Qhy6PJjOvERyZMMo1'
+const searchURL = 'https://developer.nps.gov/api/v1/parks/'
 
-function searchParks(){
+// Take User Input
+// Format the URL for the Fetch
+
+// Make Fetch request
+// Translate Data to JSON
+
+// Append Appropriate JSON info to DOM
+
+function formatUserInput(){
     $('#js-park-search').submit(function(e){
         e.preventDefault();
-        buildUrl();
-        fetch(url)
-            .then(response => response.json())
-            .then(responseJson => {
-                console.log(responseJson);
-            });
+        let stateSearch = $('#js-state-search').val();
+        let maxResults = $('#js-search-limit').val();
+        let newSearch = stateSearch.replace(/\s+/g,'%2C').replace(/,/g,'%2C')
+        stateSearch = "stateCode=" + newSearch
+        buildURL(stateSearch, maxResults)
     });
 }
 
-function buildUrl(){
-    let stateSearch = $('#js-state-search').val();
-    let searchLimit = $('#js-search-limit').val();
-    if(!searchLimit){
-        searchLimit = 10;
-    }
-    url = url + 'stateCode=' + stateSearch + '&limit=' + searchLimit + '&api_key=' + api_key;
-    return url;
+function buildURL(stateSearch, maxResults=10){
+    let url = searchURL + '?' + stateSearch + "&limit=" + maxResults + '&api_key=' + api_key
+    buildPageOnRequest(url)
 }
 
-$(searchParks)
+function buildPageOnRequest(url){
+    console.log(url)
+    fetch(url)
+        .then(response => response.json())
+        .then(responseJson =>{
+            for(let i = 0; i < responseJson.length; i++){
+                let parkName = responseJson[i].name
+                let parkDescription = responseJson[i].description
+                let parkURL = responseJson[i].url
+                $('#js-results').append(
+                    `
+                    <li class="park-result">
+                        <h1 class="park-name">${parkName}</h1>
+                        <p class="park-description>${parkDescription}</p>
+                        <a class="park-url" href="${parkURL}">Visit Website</a>
+                    <li>
+                    `
+                )
+            }
+        })
+        .catch((error) => console.log(error));
+}
+
+$(formatUserInput)
